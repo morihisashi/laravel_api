8ffbb5f5003db3dbe2900618d451c526f883559c
morihisashi <hisa.1995.322@outlook.com>
2024-08-03 22:04:06 +0900

    joke APIの練習

--- public/api_sample1.php
+++ public/api_sample1.php
@@ -1,55 +1,37 @@
 <?php
+// APIアクセスURL 英語のjokeをランダムに出すAPI
+// $url = 'https://umayadia-apisample.azurewebsites.net/api/persons';
+$url = 'https://official-joke-api.appspot.com/jokes/random';
 
-use Illuminate\Contracts\Http\Kernel;
-use Illuminate\Http\Request;
+// ストリームコンテキストのオプションを作成
+$options = array(
+    // HTTPコンテキストオプションをセット
+    'http' => array(
+        'method'=> 'GET',
+        'header'=> 'Content-type: application/json; charset=UTF-8' //JSON形式で表示
+    )
+);
 
-define('LARAVEL_START', microtime(true));
+// ストリームコンテキストの作成
+// $context = stream_context_create($options);
 
-/*
-|--------------------------------------------------------------------------
-| Check If The Application Is Under Maintenance
-|--------------------------------------------------------------------------
-|
-| If the application is in maintenance / demo mode via the "down" command
-| we will load this file so that any pre-rendered content can be shown
-| instead of starting the framework, which could cause an exception.
-|
-*/
+$raw_data = file_get_contents($url, false, NULL);
+echo $raw_data;
+// debug
+//var_dump($raw_data);
 
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
-
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| this application. We just need to utilize it! We'll simply require it
-| into the script here so we don't need to manually load our classes.
-|
-*/
-
-require __DIR__.'/../vendor/autoload.php';
-
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request using
-| the application's HTTP kernel. Then, we will send the response back
-| to this client's browser, allowing them to enjoy our application.
-|
-*/
-
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-$kernel = $app->make(Kernel::class);
-
-$response = $kernel->handle(
-    $request = Request::capture()
-)->send();
-
-$kernel->terminate($request, $response);
+// json の内容を連想配列として $data に格納する
+$data = json_decode($raw_data,true);
+var_dump($data);
+?>
+<table>
+    <tr>
+        <th>name</th>
+        <th>note</th>
+    </tr>
+        <tr>
+            <td><?php echo $data['setup']; ?></td>
+            <td><?php echo $data['punchline']; ?></td>
+        </tr>
+</table>
+?>
