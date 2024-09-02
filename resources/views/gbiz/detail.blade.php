d76647af7bdbd2b7b7198e89bbe12cdc58a41c92
morihisashi <hisa.1995.322@outlook.com>
2024-09-02 20:36:17 +0900

    詳細情報をテーブル形式で表示しました

--- resources/views/gbiz/detail.blade.php
+++ resources/views/gbiz/detail.blade.php
@@ -12,6 +12,23 @@
         <form action="{{route('gbiz.list')}}" method="POST">
         @csrf
             <?php var_dump($detail);?>
+            <table>
+                <tr>
+                    <th>会社名</th>
+                    <th>ステータス</th>
+                    <th>住所</th>
+                    <th>法人番号</th>
+                </tr>
+                @foreach($detail as $value)
+                    <tr>
+                        @foreach($value as $key => $val)
+                            @if($key === "name" OR $key === "status" OR $key === "location" OR $key === "corporate_number")
+                                <td>{{$val}}</td>
+                            @endif
+                        @endforeach
+                    </tr>
+                @endforeach
+            </table>
             <button type="submit">検索結果へ</button>
         </form>
     </body>
